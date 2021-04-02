d76c440fd8312fec0e7ed92308edddf956635457
1866305 <1866305@students.wits.ac.za>
2021-04-02 02:51:12 +0200

    ghgh

--- app/src/main/java/com/example/myapplicationsowh6a/HomeFragment.java
+++ app/src/main/java/com/example/myapplicationsowh6a/HomeFragment.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
